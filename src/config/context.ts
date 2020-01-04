81b899530245abf818c12610c15d29e6814260d0
Balin Sinnott <sinnott.balin@gmail.com>
2020-01-04 04:11:14 -0700

    add user to context object

--- src/config/context.ts
+++ src/config/context.ts
@@ -1,11 +1,17 @@
 import { Photon } from "@prisma/photon"
+import { getUserId } from "../modules/auth"
 
 const photon = new Photon()
 
 export interface Context {
   photon: Photon
+  userId: string | null
 }
 
-export function createContext(): Context {
-  return { photon }
+export function createContext(req: any): Context {
+  const userId = getUserId(req)
+  return {
+    photon,
+    userId
+  }
 }
