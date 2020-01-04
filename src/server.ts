6f4a796e87f4a26e0e67dfee9e7f915296d8d738
Balin Sinnott <sinnott.balin@gmail.com>
2020-01-04 04:10:50 -0700

    attach graphql shield to server.  Add Context.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,23 @@
 import "./config/env"
 import express from "express"
 import { ApolloServer } from "apollo-server-express"
-import { schema } from "./config/schema"
+import { schema as baseSchema } from "./schema/schema"
 import { createContext } from "./config/context"
+import { permissions } from "./schema/permissions"
+import { applyMiddleware } from "graphql-middleware"
 
 const isProduction = process.env.NODE_ENV === "production"
 const port = isProduction ? process.env.PORT : 3000
 
 const app = express()
-const server = new ApolloServer({ schema, context: createContext })
+const schema = applyMiddleware(baseSchema, permissions)
+
+const server = new ApolloServer({
+  context: ({ req }) => {
+    return createContext(req)
+  },
+  schema
+})
 server.applyMiddleware({ app })
 
 app.listen(port, () => {
